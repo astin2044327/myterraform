666aea2871be583ddd968c746962aa70cf15c97e
astinravidahal <44469638+astin2044327@users.noreply.github.com>
2021-06-06 05:35:07 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 provider "aws" {
   region = "us-east-1"
   profile = "default"
-  access_key = "AKIARXMJ3ZTZGZIUARE4"
-  secret_key = "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"
+  access_key = ""
+  secret_key = ""
 }
